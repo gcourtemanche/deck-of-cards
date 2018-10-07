57962239eb7ca4da342a8698cc8e625a954c162d
Gabriel Courtemanche <gabriel.courtemanche@gmail.com>
2018-10-07 10:21:46 -0400

    Basic layout

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Wrapper, Button } from './styledComponents';
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Button>Deal one card</Button>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ src/App/styledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import * as styledComponents from 'styledComponents/';
+
+export const Wrapper = styled.div`
+  text-align: center;
+`;
+
+export const Button = styled(styledComponents.Button)`
+  text-transform: uppercase;
+`;
